deafaeae20068780d513525b0e24841574d665fd
Billy Davidson <billy.davids52@gmail.com>
2018-09-04 18:51:07 -0400

    added csv and json responses

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,23 @@
 class BooksController < ApplicationController
 
   def index
-    # @books = Books.all
+    @books = Books.all
 
     respond_to do |format|
       format.html
+      format.text
+
+      format.csv do
+        render plain: Book.generate_csv(@books)
+      end
+
+      format.json do
+        render json: @books
+      end
     end
   end
-
 end
+
+
+
+# ask for help on this
